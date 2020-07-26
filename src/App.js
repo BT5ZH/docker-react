aa3bd048122254652ac69b5974a41262667d5ce2
BoTingZH <540761800@.com>
2020-07-26 15:22:05 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>hi, I'm lalal</p>
+        <p>hi, I was changed on the feature brance</p>
         <a
           className="App-link"
           href="https://reactjs.org"
